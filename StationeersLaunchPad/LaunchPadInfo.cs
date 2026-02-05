c18733153ed9761d6219dc857c91ba439d5339c6
Tom Holmes <tholmes150@gmail.com>
2026-02-04 23:19:56 -0600

    bump to 0.2.24

--- StationeersLaunchPad/LaunchPadInfo.cs
+++ StationeersLaunchPad/LaunchPadInfo.cs
@@ -4,6 +4,6 @@
   {
     public const string NAME = "StationeersLaunchPad";
     public const string GUID = "stationeers.launchpad";
-    public const string VERSION = "0.2.23";
+    public const string VERSION = "0.2.24";
   }
 }
