c9dbc3601c43bcdca930a754004c63d6289c31b3
Tom Holmes <tholmes150@gmail.com>
2026-04-23 12:11:50 -0500

    Fix error when attempting to update workshop mods on startup

--- StationeersLaunchPad/Steam.cs
+++ StationeersLaunchPad/Steam.cs
@@ -60,6 +60,8 @@ public static class Steam
       foreach (var item in needsUpdate)
         Logger.Global.LogInfo($"- {item.Title} ({item.Id})");
 
+      await UniTask.SwitchToMainThread();
+
       await UniTask.WhenAll(needsUpdate.Select(item => item.DownloadAsync().AsUniTask()));
     }
 
